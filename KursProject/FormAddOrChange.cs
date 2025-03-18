b3881b512c8e865d0a690836bb6c5917d0b52f3f
Суслов Дмитрий <dsus26@mail.ru>
2025-03-18 13:56:12 +0500

    Fixed_Insert

--- KursProject/FormAddOrChange.cs
+++ KursProject/FormAddOrChange.cs
@@ -44,13 +44,13 @@ namespace KursProject
                 int newCitizenID = (int)new SqlCommand("SELECT SCOPE_IDENTITY();", гражданинTableAdapter1.Connection).ExecuteScalar();
 
 
-                заявлениеTableAdapter1.Insert(newCitizenID, MeraID, dateTimePickerDateAddAOC.Value.Date, StatReqID, textBoxCommentAOC.Text.ToString());
+                заявлениеTableAdapter1.Insert(newCitizenID, Convert.ToInt32(MeraID), dateTimePickerDateAddAOC.Value.Date, Convert.ToInt32(StatReqID), textBoxCommentAOC.Text.ToString());
                 заявлениеTableAdapter1.Fill(newDataSet.Заявление);
             }
             else
             {
-                //DataRow OldWorkersRow = newDataSet.Workers.Select("ID = '" + Convert.ToString(OldRowID) + "'")[0];
-                //workersTableAdapter1.Update(textBoxFIO.Text, BornDate.Value.Date, Convert.ToInt32(DepartamentID), Convert.ToInt32(PostID), textBox_Image.Text, Convert.ToInt32(OldWorkersRow[0]), Convert.ToString(OldWorkersRow[1]), Convert.ToDateTime(OldWorkersRow[2]), Convert.ToInt32(OldWorkersRow[3]), Convert.ToInt32(OldWorkersRow[4]));
+            //    DataRow OldWorkersRow = newDataSet.Workers.Select("ID = '" + Convert.ToString(OldRowID) + "'")[0];
+            //    workersTableAdapter1.Update(textBoxFIO.Text, BornDate.Value.Date, Convert.ToInt32(DepartamentID), Convert.ToInt32(PostID), textBox_Image.Text, Convert.ToInt32(OldWorkersRow[0]), Convert.ToString(OldWorkersRow[1]), Convert.ToDateTime(OldWorkersRow[2]), Convert.ToInt32(OldWorkersRow[3]), Convert.ToInt32(OldWorkersRow[4]));
             }
         }
     }
